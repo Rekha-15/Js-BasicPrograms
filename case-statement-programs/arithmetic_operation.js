4cbd235565060752cbf465b5ece2ed11c6a253dc
Rekha-15 <rekhapatil.1509@gmail.com>
2021-07-17 03:03:02 -0400

    modified : .

--- case-statement-programs/arithmetic_operation.js
+++ case-statement-programs/arithmetic_operation.js
@@ -4,10 +4,11 @@
  * @author Rekha
  * @since 12-07-2021
  *******************************************************************/
-
+//declaring value
  const FEET_TO_INCH = 12
  const FEET_TO_METER = 0.3048
  
+ //performing diffrent kinds of unit conversion
  function conversion(value, inputValue) {
      switch (value) {
          case 1:
@@ -21,12 +22,14 @@
      }
  }
  
+ //taking input values from user
  console.log("Enter Value For Conversion "+
  "1:Feet To Inch Conversion "+
  "2:Inch To Feet Conversion "+
  "3:Feet To Meter Conversion "+
  "4:Meter To Feet Conversion")
  
+ //taking input from user
  const readline = require('readline').createInterface({
      input: process.stdin,
      output: process.stdout
